7a7923d5d58538fbf72cb6b1e83248fc8c45bbd4
erkobridee <erkobridee@users.noreply.github.com>
2023-02-08 10:36:33 +0000

    deploy: 7ac827a3020911b4133e940906c71f5c965bb453

--- assets/TailwindTypography-df1cb678.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9c904f86.js";import{c as o}from"./vendor-0568a7bc.js";const a="prose prose-sm prose-slate prose-a:no-underline hover:prose-a:underline dark:prose-invert lg:prose-base",i=({className:r,children:s})=>e("article",{className:o(a,r),children:s});export{i as T};
