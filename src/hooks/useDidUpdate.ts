0ce6ab01519e528a62566b4718b8e6f47b01dd98
erkobridee <erko.bridee@gmail.com>
2023-02-09 12:39:50 +0100

    fix(hooks): lint warning at the useDidUpdate

--- src/hooks/useDidUpdate.ts
+++ src/hooks/useDidUpdate.ts
@@ -3,13 +3,17 @@ import * as React from 'react';
 export const useDidUpdate = (fn: () => void, deps?: React.DependencyList) => {
   const firstRunRef = React.useRef(true);
 
-  React.useEffect(() => {
-    if (firstRunRef.current) {
-      firstRunRef.current = false;
-    } else {
-      fn();
-    }
-  }, deps);
+  React.useEffect(
+    () => {
+      if (firstRunRef.current) {
+        firstRunRef.current = false;
+      } else {
+        fn();
+      }
+    },
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    deps
+  );
 };
 
 export default useDidUpdate;
